dfb36b4f1eede5d5f70d04756007cdf57b3fcc85
SAE-Projet <saeprojet25@gmail.com>
2025-12-09 15:10:12 +0100

    Create labyrinthe.java

---
+++ labyrinthe.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Plateau {
+    public static int[][] creationPlateau(){
+        Random rdm = new Random();
+        int[][] plateau = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+            }
+        }
+        return plateau;
+    }
+}
