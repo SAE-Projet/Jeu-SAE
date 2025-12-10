ece39b648574ee68fcfa79773e568c0e917659c6
SAE-Projet <saeprojet25@gmail.com>
2025-12-10 17:39:59 +0100

    Update labyrinthe.java

--- labyrinthe.java
+++ labyrinthe.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Plateau {
-    public static int[][][] creationPlateau() {
+    public static int[][][] creationPlateau(ArrayList<Integer> idJoueurs){
         Random rdm = new Random();
         int[][][] plateau = new int[7][7][5];
         int nbrdm;
@@ -31,6 +33,22 @@ public class Plateau {
                 }
             }
         }
+        Collections.shuffle(idJoueurs, rdm);
+        int temp, rdmcoin = -1;
+        for (int i = 0; i < idJoueurs.size(); i++) {
+            temp = rdmcoin;
+            do {
+                rdmcoin = rdm.nextInt(4);
+            } while (temp == rdmcoin);
+            if(rdmcoin == 0)
+                plateau[0][0][4] = idJoueurs.get(i);
+            else if(rdmcoin == 1)
+                plateau[0][6][4] = idJoueurs.get(i);
+            else if(rdmcoin == 2)
+                plateau[6][0][4] = idJoueurs.get(i);
+            else
+                plateau[6][6][4] = idJoueurs.get(i);
+        }
         return plateau;
     }
 
@@ -53,7 +71,7 @@ public class Plateau {
                 if (plateau[i][j][4] == 0)
                     System.out.print(" ");
                 else
-                    System.out.print("#");
+                    System.out.print(">");
                 if (plateau[i][j][2] == 0)
                     System.out.print(" ");
                 else
