4666a317f8e282b717cc83a981f8551b7b5b44ff
sfwn212 <safwane.bouchikh@gmail.com>
2025-12-19 15:00:42 +0100

    Create items.java

---
+++ items.java
@@ -0,0 +1,35 @@
+public class Items {
+
+    // Initialisation des objets (S)
+    public static final int TOMATE = -2;
+    public static final int ORANGE = -3;
+    public static final int BANANE = -4;
+    public static final int MAIS = -5;
+    public static final int POIRE = -6;
+    public static final int KIWI = -7;
+    public static final int MYRTILLE = -8;
+    public static final int RAISIN = -9;
+    public static final int COCONUT = -10;
+    public static final int CHAMPIGNON = -11;
+    public static final int CROISSANT = -12;
+    public static final int FROMAGE = -13;
+    public static final int VIANDE = -14;
+    public static final int BURGIR = -15;
+    public static final int FRITES = -16;
+    public static final int PIZZA = -17;
+    public static final int SALADE = -18;
+    public static final int RAMEN = -19;
+    public static final int CREVETTE = -20;
+    public static final int ONIGIRI = -21;
+    public static final int CUPCAKE = -22;
+    public static final int DONUT = -23;
+    public static final int JUS = -24;
+    public static final int BIERE = -25;
+
+    // Affichage des objets avec des emojis (S)
+    public static final String[] EMOJIS = {"\uD83C\uDF45", "\uD83C\uDF4A", "\uD83C\uDF4C", "\uD83C\uDF3D", "\uD83C\uDF50",
+            "\uD83E\uDD5D", "\uD83E\uDED0", "\uD83C\uDF47", "\uD83E\uDD65", "\uD83C\uDF44", "\uD83E\uDD50", "\uD83E\uDDC0",
+            "\uD83C\uDF56", "\uD83C\uDF54", "\uD83C\uDF5F", "\uD83C\uDF55", "\uD83E\uDD57", "\uD83C\uDF5C", "\uD83C\uDF64",
+            "\uD83C\uDF59", "\uD83E\uDDC1", "\uD83C\uDF69", "\uD83E\uDDC3", "\uD83C\uDF7A"};
+
+}
